52a71e671dadfa7a17b854f4afa2cba5873143f7
WalakCode <gilhansen96@gmail.com>
2023-09-26 08:40:43 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+// const dbconnection = require('./config/dbC');
+// const userController = require('./controllers/userController');
+const bodyParser = require('body-parser');
+
+
+app.listen(3000)
+
+app.disable('x-powered-by');
+
+app.set('view engine','ejs')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+
+
